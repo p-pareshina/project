ed34edccc2ba365bf5c9540e43ab025e1577abd8
Polina <72819722+p-pareshina@users.noreply.github.com>
2021-12-04 12:32:04 +0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, SilentUpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView, SilentUpdateView
 from django.urls import reverse_lazy
 from .models import Task
 from django.contrib.auth.views import LoginView
